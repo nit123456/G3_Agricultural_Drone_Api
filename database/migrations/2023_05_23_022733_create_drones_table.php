e148928b16249bf53c3695eccc4b489960ec4bb6
SOCHEAT.LUN <SOCHEAT.LUN@PNC.LAN>
2023-05-23 18:48:23 +0700

    [#3]create table and connet database

---
+++ database/migrations/2023_05_23_022733_create_drones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drones', function (Blueprint $table) {
+            $table->id();
+            $table->string('codename');
+            $table->string('type');
+            $table->integer('strength');
+            $table->integer('battery');
+            $table->unsignedBigInteger('location_id');
+            $table->foreign('location_id')->references('id')->on('locations')->onDelete('cascade');
+            $table->unsignedBigInteger('farmer_id');
+            $table->foreign('farmer_id')->references('id')->on('farmers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drones');
+    }
+};
