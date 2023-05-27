c2eecda9f41ea83cdc6abae48510d506c5487a2d
nit123456 <nit.noem.@student.passerellesnumeriques.org>
2023-05-27 15:35:38 +0700

    [#16] nothing just commit to pull

--- routes/api.php
+++ routes/api.php
@@ -76,36 +76,4 @@ Route::delete('/locations/{id}', [LocationController::class, 'destroy']);
 Route::post('/login', [AuthenticationController::class, 'login']);
 Route::post('/register', [AuthenticationController::class, 'register']);
 
-//drones routes
-
-Route::get('/drones/{codename}' , [DroneController::class,'getDroneBy']);
-Route::get('/drones/{codename}/{location}' , [DroneController::class,'showDroneLocation']);
-// maps
-Route::get('/maps' , [MapController::class,'index']);
-Route::post('/map' , [MapController::class,'store']);
-
-    Route::get('/drones' , [DroneController::class,'index']);
-    Route::post('/drone' , [DroneController::class,'store']);
-    Route::get('/drone/{id}', [DroneController::class, 'droneByID']);
-    Route::put('/drone/{id}', [DroneController::class, 'updateDrone']);
-    Route::get('/drones/{codename}' , [DroneController::class,'getDroneBy']);
-    Route::get('/drones/{codename}/{location}' , [DroneController::class,'showDroneLocation']);
-
-    Route::get('/maps' , [MapController::class,'index']);
-    Route::post('/map' , [MapController::class,'store']);
-
-    Route::get('/farms' , [FarmController::class,'index']);
-    Route::post('/farm' , [FarmController::class,'store']);
-
-    Route::post('/user', [UserController::class, 'store']);
-    Route::get('/users', [UserController::class, 'index']);
-
-    Route::get('/plans', [PlanController::class, 'index']);
-    Route::post('/plans/plan', [PlanController::class, 'store']);
-
-    Route::put('/instruction/{id}', [InstructionController::class, 'update']);
-    Route::post('/instruction', [InstructionController::class, 'store']);
-
-Route::get('/locations', [LocationController::class, 'index']);
-Route::post('/location', [LocationController::class, 'store']);
 
